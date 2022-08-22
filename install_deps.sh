b414d00e032e775d95a794374ac0794d23321646
Karl Janson <karl.janson@taltech.ee>
2022-08-22 07:36:30 +0300

    updated dependencies

--- install_deps.sh
+++ install_deps.sh
@@ -1,5 +1,5 @@
 conda create --name finest python=3.8
 conda activate finest
 
-conda install -c conda-forge ros-rosbag  
-pip install Pillow argparse progrssbar2 opencv-python
+conda install -c conda-forge ros-rosbag
+pip install Pillow argparse progrssbar2 opencv-python mysql-connector
